885db074118c83753c42534f0c6a6faa61a4220d
shingo45endo <shingo45endo@gmail.com>
2019-08-30 20:01:56 +0900

    The first commit

---
+++ formatters.js
@@ -0,0 +1,39 @@
+function sint7offset6(value) {
+	const v = Number(value) - 64;
+	return `${'-±+'[Math.sign(v) + 1]}${Math.abs(v)}`;
+}
+
+function mask7(value) {
+	return value & 0x7f;
+}
+
+function panpot(value) {
+	return sint7offset6(value).replace(/^-/u, 'L').replace(/^\+/u, 'R');
+}
+
+function panpotR(value) {
+	return (value !== 0) ? panpot(value) : 'RND';
+}
+
+function noteName(value) {
+	const octave = Math.trunc(value / 12) - 1;
+	const name = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'][value % 12];
+	return `${name}${octave}`;
+}
+
+function noteNameR(value) {
+	return noteName(value);
+}
+
+function noteNameY(value) {
+	return noteName(value - 12);
+}
+
+function onOff(value) {
+	return (value) ? 'On' : 'Off';
+}
+
+export const formatters = {
+	sint7offset6, mask7, panpot, panpotR, noteNameR, noteNameY, onOff,
+};
+Object.freeze(formatters);
