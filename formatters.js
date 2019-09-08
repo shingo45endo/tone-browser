42c3c863032a994ca683bba48aa94e9137854213
shingo45endo <shingo45endo@gmail.com>
2019-09-08 21:11:43 +0900

    Add and apply new formatters for MU

--- formatters.js
+++ formatters.js
@@ -39,7 +39,28 @@ function onOff(value) {
 	return (value) ? 'On' : 'Off';
 }
 
+function lfoWaveMU(value) {
+	return {0: 'SAW', 1: 'TRI', 2: 'S&H'}[value] || '-';
+}
+
+function lfoWaveTG(value) {
+	return {0: 'SAW+', 1: 'TRI+', 2: 'SQU+', 3: 'SAW-', 4: 'TRI-', 5: 'SQU-', 6: 'S&H'}[value] || '-';
+}
+
+function pitchScalingRateMU(value) {
+	return `${{0: 100, 1: 50, 2: 20, 3: 10, 4: 5, 5: 0}[value] || '-'}%`;
+}
+
+function pitchEGDepthMU(value) {
+	return {0: '1/2', 1: '1', 2: '2', 3: '4'}[value] || '-';
+}
+
+function tonePanMU(value) {
+	return {0: 'L7', 1: 'L6', 2: 'L5', 3: 'L4', 4: 'L3', 5: 'L2', 6: 'L1', 7: '±0', 8: 'R1', 9: 'R2', 10: 'R3', 11: 'R4', 12: 'R5', 13: 'R6', 14: 'R7', 15: 'scaling'}[value] || '-';
+}
+
 export const formatters = {
 	sint7offset6, mask7, sint8, panpot, panpotR, noteNameR, noteNameY, onOff,
+	lfoWaveMU, lfoWaveTG, pitchScalingRateMU, pitchEGDepthMU, tonePanMU,
 };
 Object.freeze(formatters);
